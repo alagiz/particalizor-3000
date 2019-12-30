1aff6b50ed2115eb07fff7b3a36deee220119145
ArtemAlagizov <alagizov@gmail.com>
2019-12-30 17:16:48 +0100

    removed unused code and assets

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 import MovingPicture from "./components/moving-picture/container/MovingPicture";
-
-// this is just for development, comment out when releasing
-ReactDOM.render(<App />, document.getElementById("root"));
 
 export default MovingPicture;
