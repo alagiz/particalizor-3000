31cd65bf28950209a3f3893f3a0e246b90fbaeda
alagiz <alagizov@gmail.com>
2020-01-10 13:38:44 +0100

    minor change

--- src/components/moving-picture/container/MovingPicture.spec.tsx
+++ src/components/moving-picture/container/MovingPicture.spec.tsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import MovingPicture from "./MovingPicture";
 import MovingPictureView from "../view/MovingPictureView";
 
 describe("Given a MovingPicture", () => {
   describe("when rendering", () => {
-    it("should match snapshot", () => {
-      const component = shallow(<MovingPicture imageSource={""} />);
-
-      expect(component).toMatchSnapshot();
-    });
     it("should render MovingPictureView", () => {
       const component = mount(<MovingPicture imageSource={""} />);
 
