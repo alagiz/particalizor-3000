22253fca653543a8200ab48581889e74210b72c5
alagiz <alagizov@gmail.com>
2020-01-10 14:00:31 +0100

    added tests back

--- src/components/moving-picture/container/MovingPicture.spec.tsx
+++ src/components/moving-picture/container/MovingPicture.spec.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import MovingPicture from "./MovingPicture";
 import MovingPictureView from "../view/MovingPictureView";
 
 describe("Given a MovingPicture", () => {
   describe("when rendering", () => {
+    it("should match snapshot", () => {
+      Math.random = jest.fn(() => 777);
+
+      const component = shallow(<MovingPicture imageSource={""} />);
+
+      expect(component).toMatchSnapshot();
+    });
     it("should render MovingPictureView", () => {
       const component = mount(<MovingPicture imageSource={""} />);
 
--- src/components/particle-vortex/container/ParticleVortex.spec.tsx
+++ src/components/particle-vortex/container/ParticleVortex.spec.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 import ParticleVortex from "./ParticleVortex";
 import ParticleVortexView from "../view/ParticleVortexView";
 
 describe("Given a ParticleVortex", () => {
   describe("when rendering", () => {
+    it("should match snapshot", () => {
+      Math.random = jest.fn(() => 777);
+
+      const component = shallow(
+        <ParticleVortex
+          imageWidth={100}
+          imageHeight={100}
+          vortexNumber={1}
+          key={1}
+        />
+      );
+
+      expect(component).toMatchSnapshot();
+    });
     it("should render ParticleVortexView", () => {
       const component = mount(
         <ParticleVortex imageWidth={100} imageHeight={100} vortexNumber={1} />
