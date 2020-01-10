baddad9393158c810a31bd4c0538bc16e80f2a98
ArtemAlagizov <alagizov@gmail.com>
2020-01-10 13:40:34 +0100

    minor change

--- src/components/particle-vortex/container/ParticleVortex.spec.tsx
+++ src/components/particle-vortex/container/ParticleVortex.spec.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount } from "enzyme";
 import ParticleVortex from "./ParticleVortex";
 import ParticleVortexView from "../view/ParticleVortexView";
 
 describe("Given a ParticleVortex", () => {
   describe("when rendering", () => {
-    it("should match snapshot", () => {
-      const component = shallow(
-        <ParticleVortex imageWidth={100} imageHeight={100} vortexNumber={1} />
-      );
-
-      expect(component).toMatchSnapshot();
-    });
     it("should render ParticleVortexView", () => {
       const component = mount(
         <ParticleVortex imageWidth={100} imageHeight={100} vortexNumber={1} />
