a1e470fae8a450d9a08eb3e471993a7a80721bcc
artem <alagizov@gmail.com>
2021-05-06 14:47:40 +0200

    added test-app

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,8 @@
 import MovingPicture from "./components/moving-picture/container/MovingPicture";
-import ParticleVortex from "./components/particle-vortex/container/ParticleVortex";
+import {ParticleVortex} from "./components/particle-vortex/container/ParticleVortex";
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(<ParticleVortex/>, document.getElementById("root"));
 
 export { MovingPicture, ParticleVortex };
